9a0964a7cbf824449731af5bb530adbc66fc0815
Prashant Singh <prashantsingh896@gmail.com>
2023-02-10 20:03:49 +0530

    add mongo atlas URI for hosting

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/polling_api_db");
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 const db = require('./config/mongoose');
 const bodyParser = require('body-parser');
@@ -7,7 +8,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 //route all req to routes
 app.use('/', require('./routes'));
-
 
 app.listen(PORT, function(){
     console.log('Server listening on port: ',PORT);
