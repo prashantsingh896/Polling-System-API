9854198bba4db30a19517085e533235b693b54d3
Prashant Singh <prashantsingh896@gmail.com>
2023-02-10 20:38:39 +0530

    add home controller

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/homepage');
 
+router.get('/',controller.homepage);
 router.use('/questions',require('./question'));
 router.use('/options',require('./option'));
 
